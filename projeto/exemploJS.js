e33b8aca3f88539b8cdc44d8980b6bfd9d05eadd
João Victor <joaovictorgoncalvesgandin@gmail.com>
2025-10-04 14:16:30 -0300

    upd

---
+++ projeto/exemploJS.js
@@ -0,0 +1,44 @@
+//variaveis
+
+// as variaves let podem mudar o valor definido a elas
+// let boasVindas = "Fala, Dev!";
+// boasVindas = "Fala, Dev! Beleza?";
+
+// as variaves const NÃO podem mudar o valor definido a elas
+// const serHumano = true;
+// serHumano = false; // erro
+
+// String
+// ""
+// ""
+// ``
+
+// Number
+// 1
+// 1.2
+
+// Boolean
+// true
+// false
+
+//funções
+
+//usando uma função
+// alert("Fala, Dev!")
+
+// criando uma função
+// function sayHello() {
+//     alert("Olá")
+// }
+
+//criando uma função com argumento
+// function sayHello(name) {
+//     alert("Olá" + name)
+// }
+
+//function soma(a, b) {
+//     return a + b
+// }
+// esse return retorna o valor no console, se quiser usar para um alerta é assim
+//let resultado = soma(3, 56)
+// alert(resultado)
